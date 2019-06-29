ab9451c8227244c0c9ddf85ce9d8a2d7e08c821e
Krunoslav Uzelac <krunoslav.uzelac@leonteq.com>
2019-06-29 15:03:43 +0200

    Replacing deprecated Backoff with BackoffOpts

--- src/main/scala/com/ingenuiq/note/utils/BackoffActorHelper.scala
+++ src/main/scala/com/ingenuiq/note/utils/BackoffActorHelper.scala
@@ -1,20 +1,20 @@
 package com.ingenuiq.note.utils
 import akka.actor.Props
-import akka.pattern.{ Backoff, BackoffSupervisor }
+import akka.pattern.{BackoffOpts, BackoffSupervisor}
+
 import scala.concurrent.duration._
 
 trait BackoffActorHelper {
 
   def backoffActor(childName: String, props: Props): Props =
     BackoffSupervisor.props(
-      Backoff
+      BackoffOpts
         .onStop(
           props,
           childName      = childName,
           minBackoff     = 3.seconds,
           maxBackoff     = 30.seconds,
           randomFactor   = 0.2, // adds 20% "noise" to vary the intervals slightly
-          maxNrOfRetries = -1
         )
         .withDefaultStoppingStrategy // Stop at any Exception thrown
     )
